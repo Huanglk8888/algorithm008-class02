b9ebcba763068180dd3ef4f3906f1072a13ab14c
huanglk <463927289@qq.com>
2020-05-10 20:25:27 +0800

    第三周算法作业

---
+++ Week_03/Lowest_common_ancestor_of_a_binary_tree.java
@@ -0,0 +1,21 @@
+//236. 二叉树的最近公共祖先
+public class Lowest_common_ancestor_of_a_binary_tree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ Week_03/Majority_element.java
@@ -0,0 +1,15 @@
+//169. 多数元素
+public class Majority_element {
+    public int majorityElement(int[] nums) {
+        int cand_num = nums[0], count = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if (cand_num == nums[i])
+                ++count;
+            else if (--count == 0) {
+                cand_num = nums[i];
+                count = 1;
+            }
+        }
+        return cand_num;
+    }
+}
